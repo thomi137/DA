4d816ebd503d6aefcc96121c83c2f90a81c0c4f2
Thomas Prosser <tp@thomit.com>
2025-10-06 17:43:14 +0200

    try doing stuff in Rust

---
+++ bec_rust/target/debug/bec_rust.d
@@ -0,0 +1,1 @@
+/Users/tprosser/Projects/DA/bec_rust/target/debug/bec_rust: /Users/tprosser/Projects/DA/bec_rust/src/main.rs
---
+++ bec_rust/target/debug/deps/bec_rust-4786ed5196f66c49.d
@@ -0,0 +1,5 @@
+/Users/tprosser/Projects/DA/bec_rust/target/debug/deps/bec_rust-4786ed5196f66c49: src/main.rs
+
+/Users/tprosser/Projects/DA/bec_rust/target/debug/deps/bec_rust-4786ed5196f66c49.d: src/main.rs
+
+src/main.rs:
